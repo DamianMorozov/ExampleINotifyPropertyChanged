3f21af0198a3e921604fcd55044e039a39f6fd6a
DamianMorozov <web@morozov33.ru>
2018-09-13 12:12:20 +0300

    Work example

---
+++ ExampleINotifyPropertyChanged/DemoCustomer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ExampleINotifyPropertyChanged
+{
+    // This is a simple customer class that implements the IPropertyChange interface. 
+    public class DemoCustomer : INotifyPropertyChanged
+    {
+        // These fields hold the values for the public properties.
+        private string customerNameValue = string.Empty;
+        private string phoneNumberValue = string.Empty;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This method is called by the Set accessor of each property.  
+        // The CallerMemberName attribute that is applied to the optional propertyName  
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // The constructor is private to enforce the factory pattern.
+        private DemoCustomer()
+        {
+            customerNameValue = @"New Customer";
+            phoneNumberValue = @"(000) 000-0000";
+        }
+
+        // This is the public factory method.
+        public static DemoCustomer CreateNewCustomer()
+        {
+            return new DemoCustomer();
+        }
+
+        // This property represents an ID, suitable for use as a primary key in a database. 
+        public Guid Id { get; } = Guid.NewGuid();
+
+        public string CustomerName
+        {
+            get => customerNameValue;
+            set
+            {
+                if (value != customerNameValue)
+                {
+                    customerNameValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => phoneNumberValue;
+            set
+            {
+                if (value != phoneNumberValue)
+                {
+                    phoneNumberValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
---
+++ ExampleINotifyPropertyChanged/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ExampleINotifyPropertyChanged
+{
+    // This form demonstrates using a BindingSource to bind  
+    // a list to a DataGridView control. The list does not  
+    // raise change notifications. However the DemoCustomer type   
+    // in the list does.
+    public partial class Form1 : Form
+    {
+        // This button causes the value of a list element to be changed.  
+        private Button buttonChangeItem = new Button();
+
+        // Field for new customer's values.
+        private TextBox textBoxCustomerName = new TextBox();
+
+        // Field for new customer's values.
+        private TextBox textBoxCustomerPhone = new TextBox();
+
+        // This DataGridView control displays the contents of the list.  
+        private DataGridView dataGridViewCustomer = new DataGridView();
+
+        // This BindingSource binds the list to the DataGridView control.  
+        private BindingSource bindingSourceCustomer = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set up the textBoxCustomerName
+            textBoxCustomerName.Text = @"New Name";
+            textBoxCustomerName.Dock = DockStyle.Bottom;
+            Controls.Add(textBoxCustomerName);
+
+            // Set up the textBoxCustomerPhone
+            textBoxCustomerPhone.Text = @"(111) 111-2222";
+            textBoxCustomerPhone.Dock = DockStyle.Bottom;
+            Controls.Add(textBoxCustomerPhone);
+
+            // Set up the buttonChangeItem.
+            buttonChangeItem.Text = @"Change item";
+            buttonChangeItem.Dock = DockStyle.Bottom;
+            buttonChangeItem.Click += new EventHandler(changeItemBtn_Click);
+            Controls.Add(buttonChangeItem);
+
+            // Set up the dataGridViewCustomer
+            dataGridViewCustomer.Dock = DockStyle.Fill;
+            Controls.Add(dataGridViewCustomer);
+
+            Size = new Size(400, 200);
+            buttonChangeItem.Select();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create and populate the list of DemoCustomer objects  
+            // which will supply data to the DataGridView.
+            var customerList = new BindingList<DemoCustomer>
+            {
+                DemoCustomer.CreateNewCustomer(),
+                DemoCustomer.CreateNewCustomer(),
+                DemoCustomer.CreateNewCustomer()
+            };
+
+            // Bind the list to the BindingSource.
+            bindingSourceCustomer.DataSource = customerList;
+
+            // Attach the BindingSource to the DataGridView.
+            dataGridViewCustomer.DataSource = bindingSourceCustomer;
+        }
+
+        // Change the value of the CompanyName property for the first   
+        // item in the list when the "Change Item" button is clicked.
+        private void changeItemBtn_Click(object sender, EventArgs e)
+        {
+            // Get a reference to the list from the BindingSource.
+            var customerList = bindingSourceCustomer.DataSource as BindingList<DemoCustomer>;
+
+            // Change the value of the CompanyName property for the first item in the list.
+            customerList[dataGridViewCustomer.CurrentCell.RowIndex].CustomerName = textBoxCustomerName.Text;
+            customerList[dataGridViewCustomer.CurrentCell.RowIndex].PhoneNumber = textBoxCustomerPhone.Text;
+        }
+    }
+}
